13b915d70df89a01b43598b9b865ebdeddc9ff6a
Miriam Cabadajova <miriam.cabadajova1@gmail.com>
2020-03-08 23:00:29 +0100

    add second tab

--- SecondAssignment/SecondAssignment/AppDelegate.swift
+++ SecondAssignment/SecondAssignment/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  SecondAssignment
 //
-//  Created by Miriam Cabadajová on 06/03/2020.
+//  Created by Miriam Cabadajová on 08/03/2020.
 //  Copyright © 2020 FI MU. All rights reserved.
 //
 
--- SecondAssignment/SecondAssignment/SliderViewController.swift
+++ SecondAssignment/SecondAssignment/SliderViewController.swift
@@ -2,17 +2,28 @@
 //  SliderViewController.swift
 //  SecondAssignment
 //
-//  Created by Miriam Cabadajová on 07/03/2020.
+//  Created by Miriam Cabadajová on 08/03/2020.
 //  Copyright © 2020 FI MU. All rights reserved.
 //
 
 import UIKit
 
 class SliderViewController: UIViewController {
+
+    @IBOutlet weak var modifiableView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
     
-    @IBOutlet weak var sliderView: UIView!
+    @IBOutlet weak var height: NSLayoutConstraint!
     
-    @IBAction func sliderMoved(_ slider: UISlider) {
-        print(slider.value)
+    @IBOutlet weak var viewHeightConstraint: NSLayoutConstraint!
+    //@IBAction func sliderMoved(_ slider: UISlider) {
+        
+        //height.constant = CGFloat(Int(round(slider.value)))
+    //}
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        height.constant = CGFloat(Int(round(sender.value)))
     }
 }

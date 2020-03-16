c3121b49f762226d349ee634ce2d9fb56899d56d
Miriam Cabadajova <miriam.cabadajova1@gmail.com>
2020-03-16 15:36:07 +0100

    add segmented conrol for color selection

--- ThirdAssignment/ThirdAssignment/DrawViewController.swift
+++ ThirdAssignment/ThirdAssignment/DrawViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class DrawViewController: UIViewController {
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var drawButton: UIButton!
-    
     private var drawnNumber = DrawnNumber()
     var numbersCollectionDelegate: NumbersCollectionDelegate?
     override func viewDidLoad() {
@@ -19,21 +19,29 @@ class DrawViewController: UIViewController {
         drawButton.layer.cornerRadius = 5.0
     }
     
-    @IBAction func drawButtonPressed(_ sender: UIButton) {
-        //1 for red, 2 for blue and 3 for green
-        let randomColor = Int.random(in: 1...3)
+    
+    @IBAction func segmentedControlIndexSelected(_ segmentedControl: UISegmentedControl) {
         
-        switch randomColor {
-        case 1:
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
             drawnNumber.color = UIColor(named: "customRed") ?? UIColor.red
-            drawnNumber.number = Int.random(in: 1...10)
-        case 2:
+        case 1:
             drawnNumber.color = UIColor(named: "customBlue") ?? UIColor.blue
-            drawnNumber.number = Int.random(in: 11...49)
-        case 3:
+        case 2:
             drawnNumber.color = UIColor(named: "customGreen") ?? UIColor.green
+        default:
+            print("Unexpected behaviour, using default values.")
+        }
+    }
+    
+    @IBAction func drawButtonPressed(_ sender: UIButton) {
+        switch drawnNumber.color {
+        case UIColor(named: "customRed"), UIColor.red:
+            drawnNumber.number = Int.random(in: 1...10)
+        case UIColor(named: "customBlue"), UIColor.blue:
+            drawnNumber.number = Int.random(in: 11...49)
+        case UIColor(named: "customGreen"), UIColor.green:
             drawnNumber.number = Int.random(in: 1...50)
-        
         default:
             print("Unexpected behaviour, using default values.")
         }
@@ -42,7 +50,6 @@ class DrawViewController: UIViewController {
         dismiss(animated: true, completion: nil)
         
     }
-    
 }
 
 
